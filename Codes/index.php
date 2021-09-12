ec5d06878b5b02aea18ab8e1f647c4ecd04a3e71
bupashaKhan <86923355+bupashaKhan@users.noreply.github.com>
2021-09-12 22:36:21 +0600

    Project Structure re arranged

---
+++ Codes/index.php
@@ -0,0 +1,149 @@
+<?php include 'connection.php'; ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CRICKETCLUB || Home</title>
+    <link rel="icon" href="img/favicon.png">
+    
+    <?php include 'headerlink.php'; ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    
+</head>
+
+<body>
+
+    <?php include 'header.php'; ?>
+
+    <!-- banner part start-->
+    <section class="banner_part">
+        <div class="container">
+            <div class="row align-content-center">
+                <div class="col-lg-7 col-xl-5">
+                    <div class="banner_text">
+                        <h1><span>Defend &</span><br> 
+                    Dominate</h1>
+                        <p>They are to sea i waters female from lights. Deep had divided to which had. For it saw firmament face whales.</p>
+                        <a href="MemberLoginForm.php" class="btn_1">learn more <span class="ti-angle-right"></span> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- banner part start-->
+
+    <!-- about part start-->
+    <section class="about_part">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-md-6 offset-xl-1 col-xl-4">
+                    <div class="about_img">
+                        <img src="img/about.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4">
+                    <div class="about_text">
+                        <h4>About us</h4>
+                        <h2>Welcome <br>
+                        to Cricket School</h2>
+                        <p>A created won't created subdue a every green his set which above firmament earth firmament. Seed firmament be likeness fruitful to called waters. Given great said seasons his midst beast.</p>
+                        <p>A created won't created subdue a every green his set which above firmament earth firmament. Seed firmament be likeness fruitful to called waters. </p>
+                        <a href="MemberLoginForm.php" class="btn_2">read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- about part start-->
+
+    <!-- upcoming_event part start-->
+    <section class="upcoming_event section_padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-5">
+                    <div class="section_tittle text-center">
+                        <h4>Upcoming Event</h4>
+                        <h2>Land Morning Blessed</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-xl-6">
+                    <div class="upcoming_event_1">
+                        <img src="img/upcoming_event_1.jpg" alt="#">
+                        <div class="upcoming_event_text">
+                            <div class="date">
+                                <h3>15 <span>jun</span> </h3>
+                            </div>
+                            <div class="time">
+                                <ul class="list-unstyle">
+                                    <li> <span class="ti-time"></span> 12:00 AM - 12:30 AM</li>
+                                    <li> <span class="ti-location-pin"></span> Sandis peter</li>
+                                </ul>
+                            </div>
+                            <p>Divided living they're Subdue man also dont. Land morning blessed do that for the best </p>
+                            <a href="MemberLoginForm.php" class="btn_2">View Details</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6  col-xl-6">
+                    <div class="upcoming_event_1">
+                        <img src="img/upcoming_event_2.jpg" alt="#">
+                        <div class="upcoming_event_text">
+                            <div class="date">
+                                <h3>15 <span>jun</span> </h3>
+                            </div>
+                            <div class="time">
+                                <ul class="list-unstyle">
+                                    <li> <span class="ti-time"></span> 12:00 AM - 12:30 AM</li>
+                                    <li> <span class="ti-location-pin"></span> Sandis peter</li>
+                                </ul>
+                            </div>
+                            <p>Divided living they're Subdue man also dont. Land morning blessed do that for the best </p>
+                            <a href="MemberLoginForm.php" class="btn_2">View Details</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- upcoming_event part start-->
+
+
+    <!-- about part start-->
+    <section class="about_part recreational_part">
+        <div class="container-fluid">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-md-6 offset-xl-1 col-xl-4">
+                    <div class="about_text">
+                        <h4>Recreational Program</h4>
+                        <h2>Deep which above behold an woter set a herb dry days.</h2>
+                        <p>A created won't created subdue a every green his set which above firmament earth firmament. Seed firmament be likeness fruitful to called waters. Given great said seasons his midst beast.</p>
+                        <p>A created won't created subdue a every green his set which above firmament earth firmament. Seed firmament be likeness fruitful to called waters. </p>
+                        <a href="MemberLoginForm.php" class="btn_2">read more</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4">
+                    <div class="about_img">
+                        <img src="img/recreational.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- about part start-->
+
+    
+    <?php 
+        include 'footer.php'; 
+        include 'footerlink.php'; 
+    ?>
+
+</body>
+
+</html>
